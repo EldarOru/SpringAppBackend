61ad06034d0fbff55e928df5d151f6f05ef6fbba
EldarOru <macdog108@gmail.com>
2022-06-22 15:14:32 +0300

    try openlayers

--- src/main/resources/static/scripts/main.js
+++ src/main/resources/static/scripts/main.js
@@ -2,4 +2,26 @@ $(document).ready(function(){
     $("p").click(function(){
         $(this).hide();
     });
+
+    $(".greetings").click(function(){
+        $(".greetings")
+            .animate({opacity: "0.1", left: "+=400"}, 1200)
+            .animate({opacity: "1", right: "+=400"}, 1200)
+            .animate({top: "0"}, "fast")
+            .slideUp()
+        return false;
+    });
+});
+
+const map = new ol.Map({
+    target: 'map',
+    layers: [
+        new ol.layer.Tile({
+            source: new ol.source.OSM()
+        })
+    ],
+    view: new ol.View({
+        center: ol.proj.fromLonLat([37.41, 8.82]),
+        zoom: 4
+    })
 });
